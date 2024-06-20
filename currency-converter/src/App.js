f441f6ef5a1fc1461b00d92e716f0dc76c3ca7f2
Michał <michal.klepner1@o2.pl>
2024-06-20 11:48:13 +0200

    version 1

---
+++ currency-converter/src/App.js
@@ -0,0 +1,60 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [fromCurr, setFromCurr] = useState("USD");
+  const [toCurr, setToCurr] = useState("EUR");
+  const [amount, setAmount] = useState(1);
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function convert() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurr}&to=${toCurr}`
+        );
+        const data = await res.json();
+        setConverted(data.rates[toCurr]);
+        setIsLoading(false);
+      }
+      if (fromCurr === toCurr) return setConverted(amount);
+      convert();
+    },
+    [amount, fromCurr, toCurr]
+  );
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        value={fromCurr}
+        onChange={(e) => setFromCurr(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={toCurr}
+        onChange={(e) => setToCurr(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>
+        {converted} {toCurr}
+      </p>
+    </div>
+  );
+}
